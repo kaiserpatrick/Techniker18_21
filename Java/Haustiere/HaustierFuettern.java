2d74b1b25d60b212c7cf9205278071e33a9d2c23
Patrick Kaiser <kaiser.patrick@posteo.de>
2020-05-19 16:39:35 +0200

    Haustiere erweitert

--- Java/Haustiere/HaustierFuettern.java
+++ Java/Haustiere/HaustierFuettern.java
@@ -3,16 +3,31 @@ package Java.Haustiere;
 class HaustierFuettern{
   
   public static void main(String[] args){
+
+    Haustiere[] hFeld= new Haustiere[5];
+
+    hFeld[0] = new Katze("Minka", 1.0, 1);
+    hFeld[1] = new Hund("Rambo", 9.0, 3);
+    hFeld[2] = new Hund("Isi", 4.0,2);
+    hFeld[3] = new Katze("Morle", 1.5, 2);
+    hFeld[4] = new Hund("Strolchie", 3.0, 1);
+
+    for(int i = 0; i < hFeld.length; i++) {
+      hFeld[i].fuettern();
+      hFeld[i].anzeigen();
+    }
     
-    Katze minka = new Katze("Minka", 5.0, 1); 
-    minka.fuettern();
-    minka.anzeigen();
+    /*
+      Katze minka = new Katze("Minka", 5.0, 1); 
+      minka.fuettern();
+      minka.anzeigen();
+      
+      System.out.println();    //Leerzeile
     
-    System.out.println();    //Leerzeile
-  
-    Hund strolchie = new Hund("Strolchie", 5.0, 1);
-    strolchie.fuettern();
-    strolchie.anzeigen();
+      Hund strolchie = new Hund("Strolchie", 5.0, 1);
+      strolchie.fuettern();
+      strolchie.anzeigen();
+    */
   }
   
 }
--- Java/Haustiere/Hund.java
+++ Java/Haustiere/Hund.java
@@ -3,20 +3,23 @@ package Java.Haustiere;
 class Hund extends Haustiere{
   // neue Instanzvariable nicht in Katze
   String kategorie;
+  double einheit;
   
   //Haustiere(String name, double futtervorrat)
-  
   Hund(String name, double futtervorrat, int kat){
     super(name,futtervorrat);
     switch (kat) {
       case  1: 
             kategorie = "Kleinhund";
+            einheit = 1.0;
         break;
       case  2: 
             kategorie = "Mittelgrosse Hunde";
+            einheit = 1.5;
         break;
       case  3:
             kategorie = "Große Hunde";
+            einheit = 2.0;
         break;
       default: 
         
@@ -25,7 +28,7 @@ class Hund extends Haustiere{
   
   public void friss(){
     System.out.println(name + ": " + futtervorrat);
-    futtervorrat = futtervorrat - 1.0; 
+    futtervorrat = futtervorrat - einheit; 
   } 
       
   public void sprich(){
